2a9222404c8ba3cc10e4393d1123e867e8d282c7
Aaron Duncan <a.duncan27@rgu.ac.uk>
2024-10-28 11:03:34 +0000

    finished lab 6

--- lab_6/contentlist.js
+++ lab_6/contentlist.js
@@ -1,4 +1,3 @@
-
 function addContent () {
 	// add a list of items to the content div
     let items = ["hewey", "dewey", "louie"];
@@ -10,10 +9,24 @@ function addContent () {
 		items_html += "<li>" + item + "</li>";
 	};
 	items_html += "</ul>";
+
+	console.log(items_html);
+
+	document.getElementById('content').innerHTML = items_html
 	
 	// using javascript
 	// 1. find the content div
 	// 2. modify its html attribute by adding items_html
 
+}
+
+function addElement () {
+	let newElement = document.getElementById('newElement').value
+
+	let newItem = "<ul><li>" + newElement + "</li></ul>"
+	
+	console.log(newItem);
+
+	document.getElementById('content').append(newItem)
 }
 
--- lab_6/scripts/timer.js
+++ lab_6/scripts/timer.js
@@ -6,12 +6,15 @@
  * @Last modified time: 14-Feb-192019
  */
 
-setCountdown(10);
+setCountdown(10, Bam);
+setCountdown(15, Boom);
+setCountdown(17, BigBadaBoom);
 
- function setCountdown(delay){
+ function setCountdown(delay, callback){
    //sets an timeout delay to display a message after the set delay
    setTimeout(function() {
      console.log("boom")
+     callback();
    }, delay*1000);
 
    //reduces the seconds left by 1
@@ -25,4 +28,19 @@ setCountdown(10);
      //if we are at 0 stop the countdown
      if(secondsleft <=0){clearInterval(counter)}
    }, 1000);
+ }
+
+function Bam(){
+     document.getElementById('alarm').append("Bam!");
+     document.getElementById("alarmimage").src = "bam.jpg";
+ }
+
+function Boom(){
+     document.getElementById('alarm').append("Boom!");
+     document.getElementById("alarmimage").src = "alarm.jpg";
+ }
+
+function BigBadaBoom(){
+     document.getElementById('alarm').append("BigBadaBoom!");
+     document.getElementById("alarmimage").src = "BigBadaBoom.jpg";
  }
